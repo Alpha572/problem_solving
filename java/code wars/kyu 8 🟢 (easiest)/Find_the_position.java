dcd7ebafab26785731e053e88e104fcd84dc9244
Alpha572 <124211056+Alpha572@users.noreply.github.com>
2025-03-15 17:51:45 +0200

    Update Find_the_position.java

--- java/code wars/kyu 8 🟢 (easiest)/Find_the_position.java
+++ java/code wars/kyu 8 🟢 (easiest)/Find_the_position.java
@@ -4,9 +4,6 @@ For example, for 'a' the position is 1, for 'z' the position is 26.
 Note: Only lowercased English letters are tested
 */
 
-package com.mycompany.hkr_tasks;
-
-
 public class Find_the_position {
 
   public static String position(char alphabet)
@@ -28,7 +25,17 @@ public class Find_the_position {
 
 }
 
+/*
+faster solution:
+public class Find_the_position{
 
+  public static String position(char alphabet)
+  {
+    return "Position of alphabet: " + ((int) alphabet - 96);
+  }
+}
+
+*/
 
     
 
